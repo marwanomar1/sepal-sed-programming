bd0fab694dbb3098a4a96d54b71a0905b04114f7
marwanomar1 <91081971+marwanomar1@users.noreply.github.com>
2025-12-12 04:35:35 -0600

    Create server.py

---
+++ src/ctf/server.py
@@ -0,0 +1,10 @@
+PROBLEMS = {
+    "sed_programming_full_context": {
+        "ground_truth_flag_path": "/flags/full_context.flag",
+        "model_output_flag_path": "/output/model.flag",
+    },
+    "sed_programming_minimal_context": {
+        "ground_truth_flag_path": "/flags/minimal_context.flag",
+        "model_output_flag_path": "/output/model.flag",
+    },
+}
